6cb0e00cc42e38f269408f47d93f625bf4cc5199
Randy Glasgow <Glasgowrandy@gmail.com>
2024-07-30 17:29:37 -0600

    Refactor ploy_io.go file and remove unused code

--- lib/ploy_io.go
+++ lib/ploy_io.go
@@ -6,18 +6,11 @@ import (
 	polygon "github.com/polygon-io/client-go/rest"
 )
 
-type PolyIo struct {
-	Client polygon.Client
-}
-
-var PolyIoClient PolyIo
-
 var PolygonClient polygon.Client
 
 func IntiPolygon() {
 	polygonApiKey := os.Getenv("POLYGON_API_KEY")
 	client := polygon.New(polygonApiKey)
 
-	PolyIoClient = PolyIo{Client: *client}
 	PolygonClient = *client
 }
